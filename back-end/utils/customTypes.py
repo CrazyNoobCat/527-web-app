b6c5d4c838ea015cd2594adf7f21957d0d6947ee
Charles Annals <charles@odin.nz>
2023-09-06 18:35:21 +1200

    back-end-template: correct movie & user types

--- back-end/utils/customTypes.py
+++ back-end/utils/customTypes.py
@@ -1,43 +1,46 @@
 # Custom types for the back-end
-from enum import Enum
 from typing import List
-
-Roles = Enum("Roles", ["ADMIN", "USER", "AWAITING_EMAIL_VERIFICATION"])
-
 
 class User:
     username: str
     email: str
     password: str
-    role: Roles
 
-    def __init__(self, username: str, email: str, password: str, role: Roles) -> None:
+    def __init__(self, username: str, email: str, password: str) -> None:
         self.username = username
         self.email = email
         self.password = password
-        self.role = role
 
 
 class Movie:
-    uuid: str
+    id: str
     title: str
-    year: int
-    director: str
+    release_date: int
     genre: List[str]
     summary: str
+    language: str
+    budget: int
+    revenue: int
+    runtime: int
 
     def __init__(
         self,
-        uuid: str,
+        id: str,
         title: str,
-        year: int,
-        director: str,
+        release_date: int,
         genre: List[str],
         summary: str,
+        language: str,
+        budget: int,
+        revenue: int,
+        runtime: int
     ) -> None:
-        self.uuid = uuid
+        self.id = id
         self.title = title
-        self.year = year
-        self.director = director
+        self.release_date = release_date
         self.genre = genre
         self.summary = summary
+        self.language = language
+        self.budget = budget
+        self.revenue = revenue
+        self.runtime = runtime
