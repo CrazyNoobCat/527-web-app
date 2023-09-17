a211fdd1df18b861865bfaab90ae13deb3032923
Charles Annals <charles@odin.nz>
2023-09-17 19:30:26 +1200

    order watchlist from oldest to newest

--- back-end/functions/users.py
+++ back-end/functions/users.py
@@ -150,6 +150,9 @@ def get_watchlist(event, context, user: User):
 
     watchlist = user.watch_list.split(",")
 
+    # Reverse watchlist so that the newest movies are first
+    watchlist.reverse()
+    
     paginated_watchlist = retrieve_paginated_list(watchlist, limit, page)
 
     if paginated_watchlist == [""]:
@@ -236,6 +239,9 @@ def get_watch_history(event, context, user: User):
     page, limit = retrieve_page_and_limit(params)
 
     history = user.watch_history.split(",")
+    # Reverse history so that the newest movies are first
+    history.reverse()
+    
     paginated_history = retrieve_paginated_list(history, limit, page)
 
     print("paginated_history: ", paginated_history)
