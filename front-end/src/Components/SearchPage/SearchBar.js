0c0022bdc2d101df21eb790681019dcc339638bd
Niamha <niamhathy@gmail.com>
2023-09-18 20:57:54 +1200

    search to be able to by genre and from home - also limiting results and going through pages

--- front-end/src/Components/SearchPage/SearchBar.js
+++ front-end/src/Components/SearchPage/SearchBar.js
@@ -1,20 +1,22 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Axios from 'axios';
 import { UserContext } from '../../UserContext/UserProvider';
-import { useLocation } from 'react-router-dom';
-
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
-  }
+}
 
 function SearchBar({ onSearch }) {
-
     console.log("SearchBar rendered");
     const query = useQuery();
-    const initialSearchTerm = query.get("query");
-    const initialSearchGenre = query.get("genre");
-    
+    const initialSearchTerm = query.get("query") || query.get("genre");
+    const initialSearchType = query.get("genre") ? 'genre' : 'title';
+    const [currentPage, setCurrentPage] = useState(1);
+    const [hasNextPage, setHasNextPage] = useState(true); 
+    const navigate = useNavigate();
+
+
 
   // CSS //
     const searchBarStyle = {
@@ -35,32 +37,47 @@ function SearchBar({ onSearch }) {
   
     //Main functioning of search bar//
     const {accessToken} = useContext(UserContext);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
+    const [searchType, setSearchType] = useState(initialSearchType);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const API_ENDPOINT = "https://api.cinemate.link/movies";
+    const BASE_API_ENDPOINT = "https://api.cinemate.link/movies";
 
     const fetchMovies = () => {
         if (!accessToken) {
             setError("Authentication failed.");
             return;
         }
-
+    
         setLoading(true); 
         setError(null);
-
-        Axios.get(API_ENDPOINT, {
+    
+        let params = {};
+    
+        if (searchType === 'title') {
+            params.title = searchTerm;
+        } else if (searchType === 'genre') {
+            params.genre = searchTerm;
+        }
+    
+        params.limit = 8;
+        params.page = currentPage;
+    
+        let endpoint = BASE_API_ENDPOINT;
+        if (searchType === 'genre') {
+            endpoint += "?genre=";
+        }
+    
+        Axios.get(endpoint, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             },
-            params: {
-                title: searchTerm,
-                limit: 10,    
-                page: 1       
-            }
+            params: params
         })
         .then(response => {
-            onSearch(searchTerm, response.data);
+            console.log("API response:", response.data); // Debug log
+            onSearch(searchTerm, searchType, response.data);
+            setHasNextPage(response.data.movies.length === 8);
             setLoading(false);
         })
         .catch(err => {
@@ -69,6 +86,29 @@ function SearchBar({ onSearch }) {
             setLoading(false);
         });
     };
+    const handleNextPage = () => {
+        console.log("Next button clicked!");
+        if (hasNextPage) {
+            setCurrentPage(prev => prev + 1);
+        }
+    }
+    
+    const handlePrevPage = () => {
+        setCurrentPage(prev => prev > 1 ? prev - 1 : 1);
+    }
+    
+    useEffect(() => {
+        if (initialSearchTerm) {
+            fetchMovies();  // this will use the state values, which we have set based on URL params
+        }
+    }, []);
+    useEffect(() => {
+        const pageFromURL = Number(query.get("page") || 1); // 
+        setCurrentPage(pageFromURL);
+    }, []);
+    useEffect(() => {
+        fetchMovies(); 
+   }, [currentPage]); 
 
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
@@ -78,10 +118,15 @@ function SearchBar({ onSearch }) {
 
     return (
         <div style={container}>
+            <select value={searchType} onChange={e => setSearchType(e.target.value)}>
+                <option value="title">By Title</option>
+                <option value="genre">By Genre</option>
+            </select>
+
             <input 
                 style={searchBarStyle}
                 type="text"
-                placeholder="Search for movies by title..."
+                placeholder={`Search for movies by ${searchType}...`}
                 value={searchTerm}
                 onChange={e => setSearchTerm(e.target.value)}
                 onKeyPress={handleKeyPress}
@@ -90,6 +135,8 @@ function SearchBar({ onSearch }) {
 
             {loading && <p>Loading...</p>}
             {error && <p>{error}</p>}
+            {currentPage > 1 && <button onClick={handlePrevPage}>Previous</button>}
+            {hasNextPage && <button onClick={handleNextPage}>Next</button>} 
         </div>
     );
 }
